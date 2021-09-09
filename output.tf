cc43f3c93bfcd581b7e38bddcf1a663c2bc24202
ayalcin <adem.yalcin@sva.de>
2021-09-09 15:19:20 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,10 +2,14 @@ output "private_key" {
   value = openstack_compute_keypair_v2.keypair.private_key
 }
 
-output "ip" {
+output "private_ip" {
   value = openstack_compute_instance_v2.instances[*].network[0].fixed_ip_v4
 }
 
-output "floating_ip" {
+output "public_ip" {
   value = openstack_compute_floatingip_associate_v2.associations[*].floating_ip
+}
+
+output "hostname" {
+  value = openstack_compute_instance_v2.instances[*].name
 }
